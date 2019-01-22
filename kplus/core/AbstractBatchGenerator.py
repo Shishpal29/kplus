fd1495f9100e4eadc572098019ceb6af4ab3d462
look4pritam <pritam.shete@gmail.com>
2019-01-22 16:32:01 +0530

    Abstract batch generator is added.

---
+++ kplus/core/AbstractBatchGenerator.py
@@ -0,0 +1,88 @@
+# MIT License
+#
+# Copyright (c) 2018
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import keras
+import cv2
+
+
+class AbstractBatchGenerator(keras.utils.Sequence):
+    """
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
+                 n_classes=10, shuffle=True):
+
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+    """
+
+    def __init__(self):
+        pass
+
+    def generate_dataset(self, source_root_dir, target_root_dir):
+        return (True)
+
+    def __len__(self):
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) *
+                               self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            X[i, ] = np.load('data/' + ID + '.npy')
+
+            # Store class
+            y[i] = self.labels[ID]
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
--- kplus/kclassifier/generate_dataset.py
+++ kplus/kclassifier/generate_dataset.py
@@ -23,3 +23,49 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import sys
+import argparse
+
+from kplus.core.AbstractBatchGenerator import AbstractBatchGenerator
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--source_root_dir',
+        type=str,
+        help='Input source directory.',
+        default='./source')
+
+    parser.add_argument(
+        '--target_root_dir',
+        type=str,
+        help='Output directory.',
+        default='./target')
+
+    return (parser.parse_args(argv))
+
+
+def main(args):
+
+    if (not args.source_root_dir):
+        raise ValueError(
+            'You must supply input source directory with --source_root_dir.')
+
+    if (not args.target_root_dir):
+        raise ValueError(
+            'You must supply output directory with --target_root_dir.')
+
+    batch_generator = AbstractBatchGenerator()
+    status = batch_generator.generate_dataset(args.source_root_dir,
+                                              args.target_root_dir)
+    if (status):
+        print('Dataset is generated at ' + args.target_root_dir)
+    else:
+        print('Error generating the dataset.')
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
